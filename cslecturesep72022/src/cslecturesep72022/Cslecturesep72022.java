fcfea169f682303d8e1f7a855866cf0b4e07ed0d
gusguidotti <112993448+gusguidotti@users.noreply.github.com>
2022-09-07 08:57:09 +0800

    Add files via upload

---
+++ cslecturesep72022/src/cslecturesep72022/Cslecturesep72022.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cslecturesep72022;
+import java.util.Scanner;
+
+/**
+ *
+ * @author TAU
+ */
+public class Cslecturesep72022 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        String a = "Gus";
+        String b = "Anton";
+        String c = "Andre";
+        
+        int ageA = 16;
+        int ageB = 15;
+        int ageC = 15;
+        
+        boolean isSingleA = false;
+        boolean isSingleB = true;
+        boolean isSingleC = true;
+        
+        int ageSum = ageA + ageB + ageC;
+        
+        
+        System.out.printf("Person 1:\n");
+        System.out.printf ("Name: ");
+        System.out.printf(a);
+        System.out.println();
+        System.out.printf("Age: ");
+        System.out.println(ageA);
+        System.out.printf("Is single: ");
+        System.out.println(isSingleA);
+        
+        System.out.println();
+        
+        System.out.printf("Person 2:\n");
+        System.out.printf ("Name: ");
+        System.out.printf(b);
+        System.out.println();
+        System.out.printf("Age: ");
+        System.out.println(ageB);
+        System.out.printf("Is single: ");
+        System.out.println(isSingleB);
+        
+        System.out.println();
+        
+        System.out.printf("Person 3:\n");
+        System.out.printf ("Name: ");
+        System.out.printf(c);
+        System.out.println();
+        System.out.printf("Age: ");
+        System.out.println(ageC);
+        System.out.printf("Is single: ");
+        System.out.println(isSingleC);
+        
+        System.out.println();
+        System.out.printf("The combined age of the three: ");
+        System.out.println(ageSum);
+        
+        if (ageC==ageB) {
+            System.out.println("Andre and Anton are the same age.");
+        
+        }
+        else {
+            System.out.println("Andre and Anton are not the same age.");
+        }
+        
+        if (ageA>)
+            
+    
+    }
